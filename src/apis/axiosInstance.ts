6b1f18655e470bdfe0441f9ca4568a5dd6f460af
minbo <smb5174@naver.com>
2024-05-26 16:44:20 +0900

    feat: 초기 세팅

---
+++ src/apis/axiosInstance.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}`,
+  headers: {
+    "Content-type": "application/json",
+  },
+  withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    if (config.data instanceof FormData) {
+      config.headers["Content-Type"] = "multipart/form-data";
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
