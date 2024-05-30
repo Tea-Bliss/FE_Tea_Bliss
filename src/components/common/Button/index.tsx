addf99406b9e7f505689bf7cd4409456ffb88b2f
Jin-Chanyong <cksdyd324@gmail.com>
2024-05-30 15:43:29 +0900

    feat: 디자인 시안에 따른 버튼 UI 구현

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -1,15 +1,38 @@
 import { ButtonHTMLAttributes } from 'react';
+import classNames from 'classnames';
 import styles from './Button.module.scss';
 
-type Shape = 'round' | 'square';
-
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  shape: Shape;
+  isCancel?: boolean;
+  width: number;
+  height: number;
 }
 
-function Button({ shape, children, ...props }: ButtonProps) {
+interface SquareButtonProps extends ButtonProps {
+  shape: 'square';
+  color: 'red' | 'green';
+}
+
+interface RoundButtonProps extends ButtonProps {
+  shape: 'round';
+  color: 'white' | 'red';
+}
+
+export default function Button({
+  shape,
+  color,
+  width,
+  height,
+  children,
+  isCancel = false,
+  ...props
+}: SquareButtonProps | RoundButtonProps) {
   return (
-    <button className={shape === 'round' ? styles.round : styles.square} {...props}>
+    <button
+      className={classNames(styles[shape], isCancel ? styles.cancel : styles[color])}
+      style={{ width: `${width}rem`, height: `${height}rem` }}
+      {...props}
+    >
       {children}
     </button>
   );
