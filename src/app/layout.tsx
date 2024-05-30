8746ee7aefbce45ad0031e16a6fae46ff2dead93
황준용 <wnsdyd3333@gmail.com>
2024-05-30 10:39:55 +0900

    feat: react-query provider 생성 및 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google';
 
+import ReactQueryProvider from '@/components/common/Provider/ReactQueryProvider';
+
 import type { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -16,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
+      </body>
     </html>
   );
 }
