93bd7106366b3ceca5b787c10b8c9e5b8c437c58
minbo <smb5174@naver.com>
2024-05-31 15:08:14 +0900

    feat: RadioInput 구현

---
+++ src/components/common/RadioInput/RadioInput.tsx
@@ -0,0 +1,33 @@
+import { ComponentProps, forwardRef } from 'react';
+
+import classNames from 'classnames/bind';
+
+import styles from '@/components/common/RadioInput/RadioInput.module.scss';
+import CheckedBox from '@/icons/checkedBox.svg';
+import UnCheckedBox from '@/icons/unCheckedBox.svg';
+
+const cn = classNames.bind(styles);
+
+interface RadioInputProps extends ComponentProps<'input'> {
+  content: string;
+  id: string;
+  className: string;
+  checked: boolean;
+  onChange: () => void;
+}
+
+export default forwardRef<HTMLInputElement, RadioInputProps>(function RadioInput(
+  { content, id, className, checked, onChange, ...props },
+  ref
+) {
+  console.log(checked);
+  return (
+    <div className={className}>
+      <label className={cn('label')} htmlFor={id} onClick={onChange}>
+        {checked ? <CheckedBox /> : <UnCheckedBox />}
+      </label>
+      <input className={cn('input')} ref={ref} {...props} type="radio" id={id} checked={checked} />
+      <p>{content}</p>
+    </div>
+  );
+});
