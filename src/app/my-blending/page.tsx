50c5851f2c9cf5d104d9818866230bd6a84b80a3
Jin-Chanyong <cksdyd324@gmail.com>
2024-05-31 16:09:39 +0900

    feat: 페이지 파일 생성

---
+++ src/app/my-blending/page.tsx
@@ -0,0 +1,8 @@
+import styles from '@/app/my-blending/MyBlendingPage.module.scss';
+import classNames from 'classnames/bind';
+
+const cn = classNames.bind(styles);
+
+export default function MyBlendingPage() {
+  return <></>;
+}
