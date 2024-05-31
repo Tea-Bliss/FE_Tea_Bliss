63da8970584ded10c57410df8a3120fc6e22243a
이지수 <144462290+elenfl0122@users.noreply.github.com>
2024-06-01 00:23:12 +0900

    feat: modal-root div 생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <div id="modal-root"></div>
       <body className={inter.className}>{children}</body>
     </html>
   );
