7689eff52cce6cf8f8fb917feb8de1427ba39e79
Jin-Chanyong <cksdyd324@gmail.com>
2024-05-31 16:21:11 +0900

    feat: TeaCard 컴포넌트 생성

--- src/app/my-blending/page.tsx
+++ src/app/my-blending/page.tsx
@@ -4,5 +4,17 @@ import classNames from 'classnames/bind';
 const cn = classNames.bind(styles);
 
 export default function MyBlendingPage() {
-  return <></>;
+  return (
+    <main>
+      {/** 분류 섹션 */}
+      <div>
+        <ol></ol>
+        <ol></ol>
+      </div>
+      {/** 카드리스트 섹션 */}
+      <div></div>
+      {/** 조합 섹션 */}
+      <div></div>
+    </main>
+  );
 }
---
+++ src/components/myBlending/TeaCard/index.tsx
@@ -0,0 +1,8 @@
+import styles from '@/components/myBlending/TeaCard/TeaCard.module.scss';
+import classNames from 'classnames/bind';
+
+const cn = classNames.bind(styles);
+
+export default function TeaCard() {
+  return <div></div>;
+}
