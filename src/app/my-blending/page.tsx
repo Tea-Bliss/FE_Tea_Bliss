c372cc22dd87f1b050a31f808184829487dddc53
Jin-Chanyong <cksdyd324@gmail.com>
2024-05-31 16:42:46 +0900

    feat: mock 데이터 생성

--- src/app/my-blending/page.tsx
+++ src/app/my-blending/page.tsx
@@ -1,7 +1,21 @@
 import styles from '@/app/my-blending/MyBlendingPage.module.scss';
 import classNames from 'classnames/bind';
+import mockImage from '/images/vanila.jpeg';
 
 const cn = classNames.bind(styles);
+
+const mockData = {
+  name: '푸에르초레인지(pu-erh chorange)',
+  description: '풍성한 초콜릿과 달콤한 오렌지, 푸에르를 곁들인 축제 간식입니다.',
+  sort: 'Pu Erh',
+  taste: ['단맛', '프루티', '쓴맛'],
+  imageSource: mockImage.src,
+};
+
+const mockDatas = Array.from({ length: 10 }, (_, i) => ({
+  ...mockData,
+  id: i + 1,
+}));
 
 export default function MyBlendingPage() {
   return (
--- src/components/myBlending/TeaCard/index.tsx
+++ src/components/myBlending/TeaCard/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import styles from '@/components/myBlending/TeaCard/TeaCard.module.scss';
 import classNames from 'classnames/bind';
 
