fbc68b566afc29694b3cd2231bc3af3f98baaba5
minbo <smb5174@naver.com>
2024-05-31 22:51:03 +0900

    refactor: Header 수정

--- src/components/page-layout/signUpLayout/signUpHeader/signUpHeader.tsx
+++ src/components/page-layout/signUpLayout/signUpHeader/signUpHeader.tsx
@@ -1,8 +1,13 @@
+import classNames from 'classnames/bind';
+
+import styles from '@/components/page-layout/signUpLayout/signUpHeader/signUpHeader.module.scss';
 import Logo from '@/images/logo.svg';
+
+const cn = classNames.bind(styles);
 
 export default function SignUpHeader() {
   return (
-    <header>
+    <header className={cn('logo')}>
       <Logo width={338} height={100} />
     </header>
   );
