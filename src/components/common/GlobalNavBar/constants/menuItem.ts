e0d176fdbaea374bbc1189a793279ae46b506a0d
황준용 <wnsdyd3333@gmail.com>
2024-05-31 18:16:58 +0900

    feat: nav 메뉴 상수화

---
+++ src/components/common/GlobalNavBar/constants/menuItem.ts
@@ -0,0 +1,7 @@
+const MENU_ITEM = [
+  { id: 1, label: '제품', href: '/product' },
+  { id: 2, label: '나만의 티 만들기', href: '/my-blending' },
+  { id: 3, label: '리뷰', href: '/reviews' },
+] as const;
+
+export default MENU_ITEM;
