35ceb2ebc1452a94c8ca1c735d0c749d0cdae297
minbo <smb5174@naver.com>
2024-06-03 18:07:21 +0900

    feat: 테스트 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import Card from '@/components/common/Card/Card';
-import Img from '@/icons/다운로드.jpg';
-
 export default function Home() {
   return (
     <>
       <main>메인페이지</main>
-      <Card isMainPageCard href="/" img={Img} price={20000} review={3} scope={3.5} title="달빛이룸" />
     </>
   );
 }
