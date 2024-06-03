ad33b533e3247f9ed8bc610ad6e2a5d296843b80
minbo <smb5174@naver.com>
2024-06-03 18:06:52 +0900

    feat: 카드 컴포넌트 구현

---
+++ src/components/common/Card/Card.tsx
@@ -0,0 +1,56 @@
+import classNames from 'classnames/bind';
+
+import Image, { StaticImageData } from 'next/image';
+import Link from 'next/link';
+
+import styles from '@/components/common/Card/Card.module.scss';
+import Cart from '@/icons/cart.svg';
+import Heart from '@/icons/heart.svg';
+
+const cn = classNames.bind(styles);
+
+interface CardProps {
+  href: string;
+  isMainPageCard?: boolean;
+  img: StaticImageData;
+  title: string;
+  price: number;
+  review: number;
+  scope: number;
+}
+
+export default function Card({ href, isMainPageCard, img, title, price, review, scope }: CardProps) {
+  return (
+    <section
+      className={cn('cardContainer', {
+        mainPageCardContainer: !isMainPageCard,
+      })}
+    >
+      <Link
+        href={href}
+        className={cn('imgBox', {
+          mainPageImgBox: !isMainPageCard,
+        })}
+      >
+        <Image src={img} alt="상품" fill />
+        <div className={cn('imgBackground')} />
+        {!isMainPageCard && <Cart fill="white" width={40} height={40} className={cn('cart')} />}
+      </Link>
+      <article
+        className={cn('cardInfoBox', {
+          mainPageCardInfoBox: !isMainPageCard,
+        })}
+      >
+        <div className={cn('cardTitleBox')}>
+          <p className={cn('title')}>{title}</p>
+          <p className={cn('price')}>{`${price}원`}</p>
+          {!isMainPageCard && <Heart className={cn('heart')} />}
+        </div>
+        <div className={cn('reviewScopeBox')}>
+          <p>{`리뷰 개수 : ${review}개`}</p>
+          <p>{`별점 : ${scope}`}</p>
+        </div>
+      </article>
+    </section>
+  );
+}
