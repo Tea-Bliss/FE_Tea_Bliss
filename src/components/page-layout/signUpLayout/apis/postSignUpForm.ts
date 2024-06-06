cb0c8b3ca2489945f33e03ad3ba903a9d1335eaa
minbo <smb5174@naver.com>
2024-06-06 17:05:27 +0900

    feat: 회원가입페이지 api 연동

---
+++ src/components/page-layout/signUpLayout/apis/postSignUpForm.ts
@@ -0,0 +1,14 @@
+import axiosInstance from '@/apis/axiosInstance';
+import SignUpFormData from '@/components/page-layout/signUpLayout/types/signUpFormData';
+
+const postSignUpForm = async (data: SignUpFormData) => {
+  const { email, nickname, password } = data;
+  const response = await axiosInstance.post('member/sign-up', {
+    email,
+    nickname,
+    password,
+  });
+  return response;
+};
+
+export default postSignUpForm;
--- src/components/page-layout/signUpLayout/signUpForm/signUpForm.tsx
+++ src/components/page-layout/signUpLayout/signUpForm/signUpForm.tsx
@@ -1,24 +1,30 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
 import classNames from 'classnames/bind';
 import { Controller, useForm } from 'react-hook-form';
 import { ZodIssueCode, z } from 'zod';
+
+import { useRouter } from 'next/navigation';
 
 import Button from '@/components/common/Button';
 import Input from '@/components/common/Input/Input';
 import Label from '@/components/common/Label/Label';
 import RadioInput from '@/components/common/RadioInput/RadioInput';
+import postSignUpForm from '@/components/page-layout/signUpLayout/apis/postSignUpForm';
 import styles from '@/components/page-layout/signUpLayout/signUpForm/signUpForm.module.scss';
+import SignUpFormData from '@/components/page-layout/signUpLayout/types/signUpFormData';
+import ROUTE from '@/constants/route';
 
 const cn = classNames.bind(styles);
 
 const schema = z
   .object({
-    email: z.string().nonempty('이메일을 입력해주세요.').email('이메일 형식으로 작성해 주세요.'),
-    nickName: z.string().nonempty('닉네임을 입력해주세요.').max(10, '10자 이하로 작성해주세요.'),
-    password: z.string().nonempty('비밀번호를 입력해주세요.').min(8, '8자 이상 입력해주세요.'),
-    checkPassword: z.string().nonempty('비밀번호 확인을 입력해주세요.'),
+    email: z.string().min(1, '이메일을 입력해주세요.').email('이메일 형식으로 작성해 주세요.'),
+    nickname: z.string().min(1, '닉네임을 입력해주세요.').max(10, '10자 이하로 작성해주세요.'),
+    password: z.string().min(1, '비밀번호를 입력해주세요.').min(8, '8자 이상 입력해주세요.'),
+    checkPassword: z.string().min(1, '비밀번호 확인을 입력해주세요.'),
     agree: z.boolean().refine((val) => val === true, '티블리스 가입 약관에 동의해야 합니다.'),
   })
   .superRefine((data, ctx) => {
@@ -31,18 +37,54 @@ const schema = z
     }
   });
 
+interface Error {
+  response: {
+    data: {
+      message: string;
+    };
+  };
+}
+
 export default function SignUpForm() {
+  const router = useRouter();
+
+  const mutation = useMutation({
+    mutationFn: (data: SignUpFormData) => postSignUpForm(data),
+    onSuccess: (response) => {
+      console.log(response);
+      router.push(ROUTE.SIGN_IN);
+    },
+    onError: (error: Error) => {
+      console.log(error);
+      if (error.response.data.message === '이미 존재하는 회원 입니다.') {
+        setError('email', {
+          message: '이미 존재하는 이메일입니다.',
+        });
+      }
+      if (error.response.data.message === '이미 존재하는 닉네임 입니다.') {
+        setError('nickname', {
+          message: '이미 존재하는 닉네임입니다.',
+        });
+      }
+    },
+  });
+
   const {
     control,
     handleSubmit,
     formState: { errors, isValid },
-  } = useForm({
+    setError,
+  } = useForm<SignUpFormData>({
     resolver: zodResolver(schema),
     mode: 'onBlur',
   });
 
+  const onSubmit = (data: SignUpFormData) => {
+    mutation.mutate(data);
+  };
+
   return (
-    <form className={cn('formBox')} onSubmit={handleSubmit((data) => console.log(data))}>
+    <form className={cn('formBox')} onSubmit={handleSubmit(onSubmit)}>
       <section className={cn('inputContainer')}>
         <article className={cn('inputBox')}>
           <Label className={cn('label')}>이메일</Label>
@@ -61,14 +103,14 @@ export default function SignUpForm() {
         <article className={cn('inputBox')}>
           <Label className={cn('label')}>닉네임</Label>
           <Controller
-            name="nickName"
+            name="nickname"
             control={control}
             defaultValue=""
             render={({ field }) => (
               <div className={cn('errorBox')}>
                 <Input {...field} placeholder="닉네임을 입력해주세요." className={cn('input')} onBlur={field.onBlur} />
-                {typeof errors.nickName?.message === 'string' && (
-                  <p className={cn('error')}>{errors.nickName.message}</p>
+                {typeof errors.nickname?.message === 'string' && (
+                  <p className={cn('error')}>{errors.nickname.message}</p>
                 )}
               </div>
             )}
