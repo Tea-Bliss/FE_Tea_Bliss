16dba14b3513f46a982b81c94e8ac58ce8c81fee
황준용 <wnsdyd3333@gmail.com>
2024-06-07 10:46:40 +0900

    feat: 드롭다운 루트 컴포넌트 생성

---
+++ src/components/common/DropDown/DropDown.tsx
@@ -0,0 +1,46 @@
+'use client';
+import { ReactNode, useState } from 'react';
+
+import { DropDownContext } from '@/components/common/DropDown/contexts/DropDownContext';
+
+interface DropDownProps {
+  children: ReactNode;
+  defaultTitle: string;
+  query?: string;
+  animation?: boolean;
+}
+
+export default function DropDown({ children, defaultTitle, query, animation = false }: DropDownProps) {
+  const [selectedItem, setSelectedItem] = useState<string>(defaultTitle);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isAnimating, setIsAnimating] = useState(false);
+  let timeout: ReturnType<typeof setTimeout>;
+
+  const handleDropDown = (animation: boolean) => {
+    setIsOpen((prev) => !prev);
+    if (animation) {
+      if (isAnimating) {
+        timeout = setTimeout(() => {
+          setIsAnimating(false);
+        }, 200);
+      } else {
+        setIsAnimating(true);
+      }
+    }
+    return () => clearTimeout(timeout);
+  };
+
+  const contextValue = {
+    query,
+    animation,
+    selectedItem,
+    setSelectedItem,
+    isOpen,
+    setIsOpen,
+    isAnimating,
+    setIsAnimating,
+    handleDropDown,
+  };
+
+  return <DropDownContext.Provider value={contextValue}>{children}</DropDownContext.Provider>;
+}
