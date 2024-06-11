6b2b7acc636fccb376ff628eba4e8be0f40294b0
minbo <smb5174@naver.com>
2024-06-11 18:10:48 +0900

    feat: 스켈레톤 적용

--- src/components/page-layout/productListLayout/ProductList/ProductList.tsx
+++ src/components/page-layout/productListLayout/ProductList/ProductList.tsx
@@ -8,6 +8,7 @@ import classNames from 'classnames/bind';
 import Card from '@/components/common/Card/Card';
 import getPagenationItems from '@/components/common/Pagenation/apis/getPagenationItems';
 import Pagination from '@/components/common/Pagenation/Pagenation';
+import Skeleton from '@/components/common/Skeleton/Skeleton';
 import { FILTER, LIMIT } from '@/components/page-layout/productListLayout/constants/index';
 import styles from '@/components/page-layout/productListLayout/ProductList/ProductList.module.scss';
 import ROUTE from '@/constants/route';
@@ -21,7 +22,7 @@ export default function ProductList() {
   const [page, setPage] = useState(1);
   const [selectedFilter, setSelectedFilter] = useState('all');
 
-  const { data } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ['items', page, selectedFilter],
     queryFn: () => getPagenationItems(selectedFilter, page, LIMIT),
     placeholderData: keepPreviousData,
@@ -52,17 +53,19 @@ export default function ProductList() {
             </p>
           ))}
         </div>
-        {finishedData?.map((item: FinishedItem) => (
-          <Card
-            key={item.id}
-            img={item.img}
-            href={item.href}
-            price={item.cost}
-            scope={item.rating}
-            review={item.review}
-            title={item.title}
-          />
-        ))}
+        {isLoading
+          ? Array.from({ length: 8 }).map((_, index) => <Skeleton key={index} className={cn('skeleton')} />)
+          : finishedData?.map((item: FinishedItem) => (
+              <Card
+                key={item.id}
+                img={item.img}
+                href={item.href}
+                price={item.cost}
+                scope={item.rating}
+                review={item.review}
+                title={item.title}
+              />
+            ))}
       </div>
       <Pagination currentPage={page} itemsPerPage={LIMIT} totalItems={data?.size} setPage={setPage} />
     </div>
