9c4677eb46c15a7286b63da719479823f9728534
Jin-Chanyong <155409604+Jin-Chanyong@users.noreply.github.com>
2024-06-13 19:34:39 +0900

    fix: 개별 고객 페이지 suspense 추가
    개별 고객 페이지 suspense 추가

--- src/app/admin/customer/detail/page.tsx
+++ src/app/admin/customer/detail/page.tsx
@@ -1,5 +1,11 @@
+import { Suspense } from 'react';
+
 import EachCustomerPage from '@/components/page-layout/adminLayout/components/AdminCustomerPage/EachCustomerPage';
 
 export default function customerDetail() {
-  return <EachCustomerPage />;
+  return (
+    <Suspense>
+      <EachCustomerPage />
+    </Suspense>
+  );
 }
