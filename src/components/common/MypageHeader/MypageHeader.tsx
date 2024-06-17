8fc92dc6f9ba768fe371385eef0814cd21b77d5b
minbo <smb5174@naver.com>
2024-06-17 22:56:10 +0900

    refactor: 컴포넌트 명 수정

--- src/components/common/MypageHeader/MypageHeader.tsx
+++ src/components/common/MypageHeader/MypageHeader.tsx
@@ -1,10 +1,10 @@
 import classNames from 'classnames/bind';
 
-import styles from '@/components/common/MypageHeader/MypageHeader.module.scss';
+import styles from '@/components/common/MyPageHeader/MyPageHeader.module.scss';
 
 const cn = classNames.bind(styles);
 
-export default function MypageHeader() {
+export default function MyPageHeader() {
   return (
     <header className={cn('header')}>
       <div className={cn('content')}>My page</div>
