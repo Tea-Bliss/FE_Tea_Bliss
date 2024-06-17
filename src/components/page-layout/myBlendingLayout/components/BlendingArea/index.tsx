88d369f2cff6682d1fccb36bf16f15cbda9ce503
Jin-Chanyong <cksdyd324@gmail.com>
2024-06-17 10:47:49 +0900

    feat: 티 블렌딩 페이지 구현

--- src/components/page-layout/myBlendingLayout/components/BlendingArea/index.tsx
+++ src/components/page-layout/myBlendingLayout/components/BlendingArea/index.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames/bind';
 
 import styles from '@/components/page-layout/myBlendingLayout/components/BlendingArea/BlendingArea.module.scss';
+import ResultArea from '@/components/page-layout/myBlendingLayout/components/BlendingArea/ResultArea';
 import SelectedTeas from '@/components/page-layout/myBlendingLayout/components/BlendingArea/SelectedTeas';
 
 const cn = classNames.bind(styles);
@@ -11,7 +12,7 @@ export default function BlendingArea() {
       <h1 className={cn('title')}>Selected tea</h1>
       <div className={cn('contents')}>
         <SelectedTeas />
-        <div>d</div>
+        <ResultArea />
       </div>
     </section>
   );
