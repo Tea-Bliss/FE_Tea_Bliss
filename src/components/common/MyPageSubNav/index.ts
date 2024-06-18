6f81499bbf9ef37d1901d20c738e20dbe6e0ab5c
minbo <smb5174@naver.com>
2024-06-18 14:55:18 +0900

    feat: 컴파운드 패턴 구현

---
+++ src/components/common/MyPageSubNav/index.ts
@@ -0,0 +1,15 @@
+import Box from '@/components/common/MyPageSubNav/Box/Box';
+import { Container } from '@/components/common/MyPageSubNav/Container/Container';
+import EnNav from '@/components/common/MyPageSubNav/EnNav/EnNav';
+import KoNav from '@/components/common/MyPageSubNav/KoNav/KoNav';
+import Root from '@/components/common/MyPageSubNav/Root/Root';
+
+const MyPageSubNav = {
+  Root,
+  Box,
+  KoNav,
+  EnNav,
+  Container,
+};
+
+export default MyPageSubNav;
