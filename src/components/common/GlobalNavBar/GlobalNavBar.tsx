cbe70fd69a02de7f5de7cc53dbcf95898c6479b1
황준용 <wnsdyd3333@gmail.com>
2024-06-20 05:41:08 +0900

    refactor: GNB 컴포넌트 디자인 수정

--- src/components/common/GlobalNavBar/GlobalNavBar.tsx
+++ src/components/common/GlobalNavBar/GlobalNavBar.tsx
@@ -2,13 +2,10 @@ import classNames from 'classnames/bind';
 
 import Link from 'next/link';
 
+import AuthButton from '@/components/common/GlobalNavBar/AuthButton/AuthButton';
+import MENU_ITEM from '@/components/common/GlobalNavBar/constants/menuItem';
 import styles from '@/components/common/GlobalNavBar/GlobalNavBar.module.scss';
 import ROUTE from '@/constants/route';
-import Logo from '@/icons/logo.svg';
-
-import AuthButton from './component/AuthButton/AuthButton';
-import Hamburger from './component/Hamburger/Hamburger';
-import MENU_ITEM from './constants/menuItem';
 
 const cn = classNames.bind(styles);
 
@@ -16,24 +13,24 @@ export default function GlobalNavBar() {
   return (
     <header className={cn('container')}>
       <nav className={cn('navContainer')}>
-        <div className={cn('leftWrapper')}>
-          <Link href={ROUTE.HOME}>
-            <Logo width="100%" height="100%" />
-          </Link>
+        <div className={cn('logoWrapper')}>
+          <h1>
+            <Link className={cn('logo')} href={ROUTE.HOME}>
+              Tea Bliss
+            </Link>
+          </h1>
+        </div>
+
+        <div className={cn('menuWrapper')}>
           <ul className={cn('menuBox')}>
             {MENU_ITEM.map((item) => (
-              <li key={item.id}>
-                <Link className={cn('menuItem')} href={item.href}>
-                  {item.label}
-                </Link>
+              <li className={cn('menuItem')} key={item.id}>
+                <Link href={item.href}>{item.label}</Link>
               </li>
             ))}
           </ul>
-        </div>
 
-        <div className={cn('rightWrapper')}>
           <AuthButton />
-          <Hamburger />
         </div>
       </nav>
     </header>
