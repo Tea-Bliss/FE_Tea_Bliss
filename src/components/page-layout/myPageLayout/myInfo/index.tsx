15dbc5cec615fe2452be20df0753cf899b7d575f
Jin-Chanyong <cksdyd324@gmail.com>
2024-06-19 14:10:23 +0900

    feat: supabase 파일 생성

--- src/components/page-layout/myPageLayout/myInfo/index.tsx
+++ src/components/page-layout/myPageLayout/myInfo/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import classNames from 'classnames/bind';
 
 import Footer from '@/components/common/Footer/Footer';
@@ -7,9 +9,14 @@ import PasswordChange from '@/components/page-layout/myPageLayout/myInfo/compone
 import ProfileChange from '@/components/page-layout/myPageLayout/myInfo/components/ProfileChange';
 import styles from '@/components/page-layout/myPageLayout/myInfo/MyInfoLayout.module.scss';
 
+import useGetUsers from './hooks/useGetUsers';
+
 const cn = classNames.bind(styles);
 
 export default function MyInfoLayout() {
+  const { data } = useGetUsers();
+
+  console.log(data);
   return (
     <>
       <GlobalNavBar />
