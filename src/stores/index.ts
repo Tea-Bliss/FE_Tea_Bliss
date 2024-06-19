782a2a5c8840b63dcd550b582dbe2d6a19bbfa2b
minbo <smb5174@naver.com>
2024-06-20 01:00:45 +0900

    feat: zustand 생성

---
+++ src/stores/index.ts
@@ -0,0 +1,19 @@
+import { Dispatch, SetStateAction } from 'react';
+
+import { create } from 'zustand';
+
+interface UserInfoState {
+  caffeine: boolean | null;
+  setCaffeine: (caffeine: boolean) => void;
+  season: string;
+  setSeason: (season: string) => void;
+}
+
+const useUserInfoStore = create<UserInfoState>((set) => ({
+  caffeine: null,
+  setCaffeine: (caffeine) => set({ caffeine: caffeine }),
+  season: '',
+  setSeason: (season) => set({ season: season }),
+}));
+
+export default useUserInfoStore;
