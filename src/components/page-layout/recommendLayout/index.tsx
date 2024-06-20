8e73c2792adce781cca5205d20cf70d4d7df165c
이지수 <144462290+elenfl0122@users.noreply.github.com>
2024-06-20 17:19:56 +0900

    feat: 추천페이지 레이아웃 구현

--- src/components/page-layout/recommendLayout/index.tsx
+++ src/components/page-layout/recommendLayout/index.tsx
@@ -1,3 +1,37 @@
-export default function RecommendLayout() {
-  return <div></div>;
+import classNames from 'classnames/bind';
+
+import Image from 'next/image';
+
+import styles from '@/components/page-layout/recommendLayout/RecommendLayout.module.scss';
+import Checklist from '@/images/checklist.png';
+
+import RecommendList from './components/RecommendList';
+import SurveyResults from './components/SurveyResults';
+
+import Banner from '@/images/surveyResultBanner.png';
+
+const cn = classNames.bind(styles);
+
+interface RecommendLayoutProps {
+  name: string;
+}
+
+export default function RecommendLayout({ name = '미치광이고양이' }: RecommendLayoutProps) {
+  return (
+    <div className={cn('layout')}>
+      <Image src={Banner} alt="설문 결과 배너이미지" />
+      <div>
+        <div className={cn('checklist')}>
+          <Image src={Checklist} alt="설문 결과" />
+          <div className={cn('results')}>
+            <SurveyResults name={name} />
+          </div>
+          I
+        </div>
+      </div>
+      <div>
+        <RecommendList />
+      </div>
+    </div>
+  );
 }
