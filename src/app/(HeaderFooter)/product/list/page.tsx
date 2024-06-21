40ffac5fb2ce8cee9ef9cdf36cf3b38789d400f6
Jin-Chanyong <155409604+Jin-Chanyong@users.noreply.github.com>
2024-06-21 13:07:39 +0900

    fix: build 오류 해결
    fix: build 오류 해결

--- src/app/(HeaderFooter)/product/list/page.tsx
+++ src/app/(HeaderFooter)/product/list/page.tsx
@@ -1,9 +1,11 @@
+import { Suspense } from 'react';
+
 import ProductListLayout from '@/components/page-layout/productListLayout/productListLayout';
 
 export default function ProductList() {
   return (
-    <>
+    <Suspense>
       <ProductListLayout />
-    </>
+    </Suspense>
   );
 }
