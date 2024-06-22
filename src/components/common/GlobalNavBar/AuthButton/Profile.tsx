11ac65d2bfa6dba1fe3cab104e5598a742a254e9
황준용 <wnsdyd3333@gmail.com>
2024-06-22 14:28:01 +0900

    feat: 유저 정보 api 연결

--- src/components/common/GlobalNavBar/AuthButton/Profile.tsx
+++ src/components/common/GlobalNavBar/AuthButton/Profile.tsx
@@ -1,12 +1,15 @@
 'use client';
 import { useRef, useState } from 'react';
 
+import { useQuery } from '@tanstack/react-query';
 import classNames from 'classnames/bind';
 
 import Image from 'next/image';
 
+import { getUserInfo } from '@/apis/getUserInfo';
 import styles from '@/components/common/GlobalNavBar/AuthButton/Profile.module.scss';
 import MyInfo from '@/components/common/GlobalNavBar/MyInfo/MyInfo';
+import { useUserInfoQuery } from '@/hooks/query/useUserInfoQuery';
 import useOutsideClick from '@/hooks/useOutsideClick';
 
 const cn = classNames.bind(styles);
@@ -15,6 +18,8 @@ export default function Profile() {
   const [showMyInfo, setShowMyInfo] = useState(false);
   const ref = useRef<HTMLDivElement | null>(null);
   const toggleRef = useRef<HTMLButtonElement | null>(null);
+  const { data } = useUserInfoQuery();
+
   const toggleMyInfo = () => {
     setShowMyInfo((prev) => !prev);
   };
@@ -24,14 +29,25 @@ export default function Profile() {
     <>
       <button onClick={toggleMyInfo} ref={toggleRef}>
         <span className={cn('profileContent')}>
-          <Image
-            src={'/icons/다운로드.jpg'}
-            width={24}
-            height={24}
-            style={{ borderRadius: '999px' }}
-            alt="프로필 이미지"
-          />
-          <span className={cn('profileName')}>황준용</span>
+          {data.profile ? (
+            <Image
+              src={data.profile}
+              width={24}
+              height={24}
+              style={{ borderRadius: '999px' }}
+              alt="유저 프로필 이미지"
+              placeholder="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII="
+            />
+          ) : (
+            <Image
+              src={'/images/default_profile.png'}
+              width={24}
+              height={24}
+              style={{ borderRadius: '999px' }}
+              alt="기본 프로필 이미지"
+            />
+          )}
+          <span className={cn('profileName')}>{data.nickname}</span>
         </span>
       </button>
       {showMyInfo && <MyInfo toggle={toggleMyInfo} ref={ref} />}
--- src/components/common/GlobalNavBar/MyInfo/MyInfoContent.tsx
+++ src/components/common/GlobalNavBar/MyInfo/MyInfoContent.tsx
@@ -6,30 +6,42 @@ import Link from 'next/link';
 import Button from '@/components/common/Button';
 import styles from '@/components/common/GlobalNavBar/MyInfo/MyInfo.module.scss';
 import ROUTE from '@/constants/route';
+import { useUserInfoQuery } from '@/hooks/query/useUserInfoQuery';
 
 const cn = classNames.bind(styles);
 
-// TODO : 로그인 정보 api 연결 예정
-
 export default function MyInfoContent() {
+  const { data } = useUserInfoQuery();
+
   return (
     <>
       <div className={cn('profile')}>
-        <Image
-          src={'/icons/다운로드.jpg'}
-          width={105}
-          height={105}
-          className={cn('profileImage')}
-          alt="프로필 이미지"
-        />
-        <p className={cn('userName')}>황준용</p>
+        {data.profile ? (
+          <Image
+            src={data.profile}
+            width={105}
+            height={105}
+            className={cn('profileImage')}
+            alt="유저 프로필 이미지"
+            placeholder="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkqAcAAIUAgUW0RjgAAAAASUVORK5CYII="
+          />
+        ) : (
+          <Image
+            src={'/images/default_profile.png'}
+            width={105}
+            height={105}
+            style={{ borderRadius: '999px' }}
+            alt="기본 프로필 이미지"
+          />
+        )}
+        <p className={cn('userName')}>{data.nickname}</p>
       </div>
       <div className={cn('buttonWrapper')}>
-        <Button shape="round" color="red">
-          <Link className={cn('myInfoBtn')} href={ROUTE.MY_PAGE}>
+        <Link className={cn('myInfoBtn')} href={ROUTE.MY_PAGE_MY_INFO}>
+          <Button shape="round" color="red">
             마이페이지
-          </Link>
-        </Button>
+          </Button>
+        </Link>
         <Button shape="round" color="white">
           <p className={cn('myInfoBtn')}>로그아웃</p>
         </Button>
