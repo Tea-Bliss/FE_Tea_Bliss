8b47131377b3727ff0f1dd7c6727b49eb62f25b6
황준용 <wnsdyd3333@gmail.com>
2024-06-22 23:51:31 +0900

    feat: 쿼리 훅 로직 수정

--- src/hooks/query/useUserInfoQuery.ts
+++ src/hooks/query/useUserInfoQuery.ts
@@ -3,5 +3,8 @@ import { useQuery } from '@tanstack/react-query';
 import { getUserInfo } from '@/apis/getUserInfo';
 
 export const useUserInfoQuery = () => {
-  return useQuery({ queryKey: ['userInfo'], queryFn: getUserInfo });
+  const isBrowser = typeof window !== 'undefined';
+  const token = isBrowser ? localStorage.getItem('token') : null;
+
+  return useQuery({ queryKey: ['userInfo'], queryFn: getUserInfo, enabled: !!token });
 };
