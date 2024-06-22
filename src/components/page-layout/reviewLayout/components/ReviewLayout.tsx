8ad1a7344e7f148fb3742814fdff2bcb7214c871
minbo <smb5174@naver.com>
2024-06-23 00:12:29 +0900

    feat: 스켈레톤 처리

--- src/components/page-layout/reviewLayout/components/ReviewLayout.tsx
+++ src/components/page-layout/reviewLayout/components/ReviewLayout.tsx
@@ -3,6 +3,7 @@
 import { useQuery } from '@tanstack/react-query';
 import classNames from 'classnames/bind';
 
+import Skeleton from '@/components/common/Skeleton/Skeleton';
 import SubHeader from '@/components/common/SubHeader/SubHeader';
 import styles from '@/components/page-layout/reviewLayout/components/ReviewLayout.module.scss';
 
@@ -13,7 +14,7 @@ import { CardData } from '../types';
 const cn = classNames.bind(styles);
 
 export default function ReviewLayout() {
-  const { data } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ['review'],
     queryFn: () => getReview(8),
   });
@@ -25,9 +26,9 @@ export default function ReviewLayout() {
         <div className={cn('box')}>
           <p className={cn('title')}>블렌딩 리뷰 추천순 top 8</p>
           <div className={cn('gridbox')}>
-            {data?.data.map((card: CardData, idx: number) => (
-              <Card data={card} key={idx} />
-            ))}
+            {isLoading
+              ? Array.from({ length: 8 }).map((_, index) => <Skeleton key={index} className={cn('skeleton')} />)
+              : data?.data.map((card: CardData) => <Card data={card} key={card.id} />)}
           </div>
         </div>
       </div>
