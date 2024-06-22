40406c468dc43149903e28e5fcfb7cf097683c9c
minbo <smb5174@naver.com>
2024-06-22 22:22:04 +0900

    feat: 리뷰페이지에서 사용하는 카드 컴포넌트 구현

---
+++ src/components/page-layout/reviewLayout/components/Card/Card.tsx
@@ -0,0 +1,37 @@
+import classNames from 'classnames/bind';
+
+import Image from 'next/image';
+
+import Star from '@/icons/star.svg';
+
+import styles from './Card.module.scss';
+import { CardData } from '../../types';
+const cn = classNames.bind(styles);
+
+interface CardProps {
+  data: CardData;
+}
+
+export default function Card({ data }: CardProps) {
+  return (
+    <div className={cn('container')}>
+      <div className={cn('imgBox')}>
+        <div className={cn('img')}>
+          <Image src={data.img} alt="상품" fill />
+        </div>
+        <div className={cn('startContainer')}>
+          <div className={cn('starBox')}>
+            {Array.from({ length: data.likes }).map((_, index) => (
+              <Star key={index} width={20} height={20} />
+            ))}
+          </div>
+          <p className={cn('contents')}>{data.contents}</p>
+        </div>
+      </div>
+      <div className={cn('titleBox')}>
+        <p className={cn('title')}>{data.title}</p>
+        <p className={cn('date')}>{data.createDt}</p>
+      </div>
+    </div>
+  );
+}
