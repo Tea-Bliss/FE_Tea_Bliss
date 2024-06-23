cff49fb4607102993ef9a1c896c8dbb46f3eb591
황준용 <wnsdyd3333@gmail.com>
2024-06-23 13:12:26 +0900

    feat: 유저 정보 api 타입 추가

---
+++ src/types/users/type.ts
@@ -0,0 +1,12 @@
+export interface UserInfo {
+  data: {
+    id: number;
+    email: string;
+    nickname: string;
+    address: string;
+    profile: string;
+    role: string;
+    reviewCount: number;
+    purchaseAmount: number;
+  };
+}
