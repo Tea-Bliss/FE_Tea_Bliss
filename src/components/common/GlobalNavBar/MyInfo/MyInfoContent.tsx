19e5a9b959e20dbbe3ceee749c5b2678934e9c87
황준용 <wnsdyd3333@gmail.com>
2024-06-23 13:14:19 +0900

    feat: 로직 수정

--- src/components/common/GlobalNavBar/MyInfo/MyInfoContent.tsx
+++ src/components/common/GlobalNavBar/MyInfo/MyInfoContent.tsx
@@ -25,7 +25,7 @@ export default function MyInfoContent({ toggle }: MyInfoContentProps) {
   return (
     <>
       <div className={cn('profile')}>
-        {data.profile ? (
+        {data?.profile ? (
           <Image
             src={data.profile}
             width={105}
@@ -43,7 +43,7 @@ export default function MyInfoContent({ toggle }: MyInfoContentProps) {
             alt="기본 프로필 이미지"
           />
         )}
-        <p className={cn('userName')}>{data.nickname}</p>
+        <p className={cn('userName')}>{data?.nickname}</p>
       </div>
       <div className={cn('buttonWrapper')}>
         <Link className={cn('myInfoBtn', 'red')} href={ROUTE.MY_PAGE_MY_INFO} onClick={toggle}>
