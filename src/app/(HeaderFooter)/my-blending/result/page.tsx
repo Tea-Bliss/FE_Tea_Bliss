837b927176ec34ec4bb22a2cf49386980581b91d
황준용 <wnsdyd3333@gmail.com>
2024-06-25 06:15:44 +0900

    feat: 블렌딩 결과 페이지

---
+++ src/app/(HeaderFooter)/my-blending/result/page.tsx
@@ -0,0 +1,5 @@
+import MyBlendingResultLayout from '@/components/page-layout/myBlendingResultLayout/MyBlendingResultLayout';
+
+export default function Result() {
+  return <MyBlendingResultLayout />;
+}
