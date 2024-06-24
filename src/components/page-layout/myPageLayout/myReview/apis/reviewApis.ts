529136c4e9e0bb6b3c209dbbb6dc6e89a829eefd
Jin-Chanyong <cksdyd324@gmail.com>
2024-06-24 19:57:36 +0900

    feat: 찜 페이지 api 관련 코드 작성

---
+++ src/components/page-layout/myPageLayout/myReview/apis/reviewApis.ts
@@ -0,0 +1,17 @@
+import axiosInstance from '@/apis/axiosInstance';
+
+export const getMyReviews = () => {
+  return axiosInstance.get('review/my-reviews');
+};
+
+export const postReview = (data: any) => {
+  return axiosInstance.post('review', data);
+};
+
+export const putReview = (data: any, reviewId: number) => {
+  return axiosInstance.put(`review/${reviewId}`, data);
+};
+
+export const getMyPurchaseDatas = () => {
+  return axiosInstance.get('/');
+};
