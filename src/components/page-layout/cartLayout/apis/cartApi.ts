d117af6b3628e7b22ae1aa8643a71b9e8ddc6177
황준용 <wnsdyd3333@gmail.com>
2024-06-24 10:56:55 +0900

    feat: cart api 파일

---
+++ src/components/page-layout/cartLayout/apis/cartApi.ts
@@ -0,0 +1,31 @@
+import axiosInstance from '@/apis/axiosInstance';
+import { getCartItems } from '@/components/page-layout/cartLayout/types/cartApiType';
+
+export const getCartItem = async () => {
+  const { data } = await axiosInstance.get<getCartItems[]>('/basket');
+  return data;
+};
+
+export const postCartItem = async (product?: string, quantity = 1, type = 'normal') => {
+  return await axiosInstance.post('/basket', {
+    product,
+    quantity,
+    type,
+  });
+};
+
+export const patchCartItem = async (product: string, quantity: number, type = 'normal', id: number) => {
+  return await axiosInstance.patch(`/basket/${id}`, {
+    product,
+    quantity,
+    type,
+  });
+};
+
+export const deleteCartItem = async (id: number | null) => {
+  return await axiosInstance.delete('/basket', { data: { id } });
+};
+
+export const deleteSelectedCartItem = async (product: { id: number }[]) => {
+  return await axiosInstance.delete('/basket/selecteddelete', { data: product });
+};
