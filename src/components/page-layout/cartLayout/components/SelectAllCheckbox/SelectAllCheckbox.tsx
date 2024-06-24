9407ccae520991db15c2d0e57a299f728507fbf8
황준용 <wnsdyd3333@gmail.com>
2024-06-24 10:58:25 +0900

    feat: 로직 추가

--- src/components/page-layout/cartLayout/components/SelectAllCheckbox/SelectAllCheckbox.tsx
+++ src/components/page-layout/cartLayout/components/SelectAllCheckbox/SelectAllCheckbox.tsx
@@ -4,16 +4,39 @@ import styles from '@/components/page-layout/cartLayout/components/SelectAllChec
 
 const cn = classNames.bind(styles);
 
-export default function SelectAllCheckbox() {
+interface SelectAllCheckboxProps {
+  isAllSelected: boolean;
+  onSelectAll: () => void;
+  openSelectedDeleteModal: () => void;
+  hasItems?: boolean;
+  hasSelectedItems: boolean;
+}
+
+export default function SelectAllCheckbox({
+  isAllSelected,
+  onSelectAll,
+  openSelectedDeleteModal,
+  hasItems,
+  hasSelectedItems,
+}: SelectAllCheckboxProps) {
   return (
     <div className={cn('container')}>
       <div className={cn('checkboxWrapper')}>
-        <input id="SelectAllCheckbox" className={cn('checkboxInput')} type="checkbox" />
+        <input
+          id="SelectAllCheckbox"
+          className={cn('checkboxInput')}
+          type="checkbox"
+          checked={isAllSelected}
+          onChange={onSelectAll}
+          disabled={!hasItems}
+        />
         <label className={cn('label')} htmlFor="SelectAllCheckbox">
           전체 선택
         </label>
       </div>
-      <button className={cn('label')}>선택 삭제</button>
+      <button className={cn('label')} onClick={openSelectedDeleteModal} disabled={!hasSelectedItems}>
+        선택 삭제
+      </button>
     </div>
   );
 }
