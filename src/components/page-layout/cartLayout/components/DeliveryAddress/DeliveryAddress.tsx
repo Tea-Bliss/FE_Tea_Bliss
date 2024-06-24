fac107affb269415b7c83fe76af8749570ca8652
황준용 <wnsdyd3333@gmail.com>
2024-06-24 10:58:57 +0900

    feat: 로직 추가

--- src/components/page-layout/cartLayout/components/DeliveryAddress/DeliveryAddress.tsx
+++ src/components/page-layout/cartLayout/components/DeliveryAddress/DeliveryAddress.tsx
@@ -1,8 +1,12 @@
 'use client';
+import { useState } from 'react';
+
 import classNames from 'classnames/bind';
 
 import Button from '@/components/common/Button';
+import BackdropModal from '@/components/common/modal/BackdropModal/BackdropModal';
 import styles from '@/components/page-layout/cartLayout/components/DeliveryAddress/DeliveryAddress.module.scss';
+import AddressChange from '@/components/page-layout/myPageLayout/myInfo/components/AddressChange';
 import { useUserInfoQuery } from '@/hooks/query/useUserInfoQuery';
 import Location from '@/icons/location.svg';
 
@@ -10,20 +14,29 @@ const cn = classNames.bind(styles);
 
 export default function DeliveryAddress() {
   const { data } = useUserInfoQuery();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    data && (
-      <div className={cn('deliveryAddress')}>
-        <div className={cn('addressHeader')}>
-          <div className={cn('locationIcon')}>
-            <Location />
-            <h3 className={cn('addressTitle')}>배송지</h3>
+    <>
+      {data && (
+        <div className={cn('deliveryAddress')}>
+          <div className={cn('addressHeader')}>
+            <div className={cn('locationIcon')}>
+              <Location />
+              <h3 className={cn('addressTitle')}>배송지</h3>
+            </div>
+            <span className={cn('addressDetails')}>{data.address}</span>
           </div>
-          <span className={cn('addressDetails')}>{data.address}</span>
+          <Button shape="square" color="white" onClick={() => setIsModalOpen(!isModalOpen)}>
+            배송지 변경
+          </Button>
         </div>
-        <Button shape="square" color="white">
-          배송지 변경
-        </Button>
-      </div>
-    )
+      )}
+      <BackdropModal isOpen={isModalOpen} onClose={() => setIsModalOpen(!isModalOpen)}>
+        <div className={cn('addressChange')}>
+          <AddressChange />
+        </div>
+      </BackdropModal>
+    </>
   );
 }
