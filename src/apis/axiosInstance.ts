2f9624ae1e82ff05503b966cdf08a15b27095824
minbo <smb5174@naver.com>
2024-06-24 22:14:05 +0900

    refactor: 인스턴스 수정

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -4,7 +4,14 @@ import { useRouter } from 'next/navigation';
 
 import ROUTE from '@/constants/route';
 
-const axiosInstance = axios.create({
+export const axiosInstance = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}`,
+  headers: {
+    'Content-type': 'application/json',
+  },
+});
+
+export const axiosAnotherInstance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}`,
   headers: {
     'Content-type': 'application/json',
@@ -26,38 +33,32 @@ axiosInstance.interceptors.request.use(
   }
 );
 
-axiosInstance.interceptors.response.use(
-  (response) => {
-    // 응답이 성공적이면 그대로 응답 반환
-    return response;
-  },
-  async (error) => {
-    const originalRequest = error.config;
+// axiosInstance.interceptors.response.use(
+//   (response) => {
+//     // 응답이 성공적이면 그대로 응답 반환
+//     return response;
+//   },
+//   async (error) => {
+//     if (error.response.status === 401) {
+//       try {
+//         // re-issue 엔드포인트를 호출하여 새로운 토큰을 가져옴
+//         const response = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}member/re-issue`, {
+//           headers: {
+//             'Content-type': 'application/json',
+//             Authorization: `${localStorage.getItem('token')}`,
+//           },
+//         });
 
-    if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
+//         if (response.status === 200) {
+//           const newToken = response.headers.authorization;
+//           localStorage.setItem('token', newToken);
+//         }
+//       } catch (err) {
+//         const router = useRouter();
+//         router.push(ROUTE.HOME);
+//       }
+//     }
 
-      try {
-        // re-issue 엔드포인트를 호출하여 새로운 토큰을 가져옴
-        const response = await axiosInstance.post('/api/member/re-issue');
-
-        if (response.status === 200) {
-          const newToken = response.headers.authorization;
-          localStorage.removeItem('token');
-          localStorage.setItem('token', newToken);
-
-          // 새로운 토큰으로 원래의 요청을 다시 시도
-          return axiosInstance(originalRequest);
-        }
-      } catch (err) {
-        // 오류 처리 (예: 로그인 페이지로 리디렉션)
-        const router = useRouter();
-        router.push(ROUTE.HOME);
-      }
-    }
-
-    return Promise.reject(error);
-  }
-);
-
-export default axiosInstance;
+//     return Promise.reject(error);
+//   }
+// );
