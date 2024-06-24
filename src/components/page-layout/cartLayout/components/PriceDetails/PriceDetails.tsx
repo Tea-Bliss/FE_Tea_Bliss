b698ad34428e7a26902784b76acb620fcd29bf5b
황준용 <wnsdyd3333@gmail.com>
2024-06-24 10:58:40 +0900

    feat: 로직 추가

--- src/components/page-layout/cartLayout/components/PriceDetails/PriceDetails.tsx
+++ src/components/page-layout/cartLayout/components/PriceDetails/PriceDetails.tsx
@@ -2,33 +2,46 @@ import classNames from 'classnames/bind';
 
 import styles from '@/components/page-layout/cartLayout/components/PriceDetails/PriceDetails.module.scss';
 
+import { CheckoutSummaryProps } from '../CheckoutSummary/CheckoutSummary';
+
 const cn = classNames.bind(styles);
 
-export default function PriceDetails() {
+interface PriceDetailsProps extends CheckoutSummaryProps {}
+
+export default function PriceDetails({ selectedItems }: PriceDetailsProps) {
+  const totalPrice = selectedItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
+  const deliveryFee = totalPrice >= 50000 ? 0 : 3000;
+  const totalAmount = totalPrice + deliveryFee;
+  const additionalOrderForFreeShipping = 50000 - totalPrice;
+
   return (
     <div className={cn('container')}>
       <div className={cn('item')}>
         <span className={cn('label')}>상품금액</span>
         <span className={cn('amount')}>
-          13,000<span className={cn('currency')}>원</span>
+          {totalPrice.toLocaleString()}
+          <span className={cn('currency')}>원</span>
         </span>
       </div>
 
       <div className={cn('item')}>
         <span className={cn('label')}>배송비</span>
         <span className={cn('amount')}>
-          +3,000<span className={cn('currency')}>원</span>
+          {deliveryFee.toLocaleString()}
+          <span className={cn('currency')}>원</span>
         </span>
       </div>
 
-      <p className={cn('additionalInfo')}>
-        2,200원 추가주문 시, <strong>무료배송</strong>
-      </p>
+      {totalPrice <= 50000 && (
+        <p className={cn('additionalInfo')}>
+          {additionalOrderForFreeShipping.toLocaleString()}원 추가주문 시, <strong>무료배송</strong>
+        </p>
+      )}
 
       <div className={cn('item', 'total-item')}>
         <span className={cn('label')}>결제예정금액</span>
         <span>
-          <strong className={cn('total-amount')}>40,800</strong>
+          <strong className={cn('total-amount')}> {totalAmount.toLocaleString()}</strong>
           <span className={cn('currency')}>원</span>
         </span>
       </div>
