b9c5b1709c6b2f079a087dc5520ef8494e5eb422
황준용 <wnsdyd3333@gmail.com>
2024-06-24 11:00:53 +0900

    feat: api 연결 및 장바구니 기능 추가

--- src/components/common/QuantityButton/QuantityButton.tsx
+++ src/components/common/QuantityButton/QuantityButton.tsx
@@ -5,20 +5,23 @@ import classNames from 'classnames/bind';
 import styles from '@/components/common/QuantityButton/QuantityButton.module.scss';
 import { getCartItems } from '@/components/page-layout/cartLayout/types/cartApiType';
 import Minus from '@/icons/minus.svg';
-import Plus from '@/icons/plus.svg';
+import Plus from '@/icons/plusIcon.svg';
 
 const cn = classNames.bind(styles);
 
-interface QuantityButton extends Pick<getCartItems, 'quantity'> {}
+interface QuantityButton extends Pick<getCartItems, 'quantity'> {
+  increase: () => void;
+  decrease: () => void;
+}
 
-export default function QuantityButton({ quantity }: QuantityButton) {
+export default function QuantityButton({ quantity, increase, decrease }: QuantityButton) {
   return (
     <div className={cn('quantity')}>
-      <button>
-        <Minus />
+      <button onClick={decrease} disabled={quantity === 1 ? true : false}>
+        <Minus stroke={quantity === 1 ? '#BFBFBF' : '#181717'} />
       </button>
       <div className={cn('quantityNumber')}>{quantity}</div>
-      <button>
+      <button onClick={increase}>
         <Plus />
       </button>
     </div>
--- src/components/page-layout/cartLayout/components/CartItem/CartItem.tsx
+++ src/components/page-layout/cartLayout/components/CartItem/CartItem.tsx
@@ -1,4 +1,6 @@
 'use client';
+import { useEffect, useState } from 'react';
+
 import classNames from 'classnames/bind';
 
 import Image from 'next/image';
@@ -7,27 +9,63 @@ import QuantityButton from '@/components/common/QuantityButton/QuantityButton';
 import styles from '@/components/page-layout/cartLayout/components/CartItem/CartItem.module.scss';
 import { getCartItems } from '@/components/page-layout/cartLayout/types/cartApiType';
 import Delete from '@/icons/close.svg';
-import { removeCartItem } from '@/lib/cartActions';
+
+import usePatchCartItemMutation from '../../hooks/usePatchCartItemMutation';
 
 const cn = classNames.bind(styles);
 
 interface CartItemProps extends getCartItems {
-  openModal: () => void;
+  openModal: (productId: number) => void;
+  handleItemSelection: (id: number, price: number, quantity: number, isSelected: boolean) => void;
+  isSelected: boolean;
 }
 
-export default function CartItem({ img, name, nameEng, price, quantity, openModal }: CartItemProps) {
+export default function CartItem({
+  id,
+  img,
+  product,
+  nameEng,
+  price,
+  quantity,
+  openModal,
+  handleItemSelection,
+  isSelected,
+}: CartItemProps) {
+  const [currentQuantity, setCurrentQuantity] = useState(quantity);
+  const { mutate } = usePatchCartItemMutation(product, currentQuantity, id);
+
+  const handleIncreaseClick = () => {
+    const updatedQuantity = currentQuantity + 1;
+    setCurrentQuantity(updatedQuantity);
+    mutate();
+  };
+
+  const handleDecreaseClick = () => {
+    const updatedQuantity = currentQuantity - 1;
+    setCurrentQuantity(updatedQuantity);
+    mutate();
+  };
+
   return (
     <li className={cn('cartItemContainer')}>
       <div className={cn('cartItem')}>
-        <input type="checkbox" className={cn('cartItemCheckbox')} />
+        <input
+          type="checkbox"
+          className={cn('cartItemCheckbox')}
+          onChange={() => handleItemSelection(id, price, quantity, isSelected)}
+          checked={isSelected}
+        />
         <Image src={img} width={64} height={80} alt="상품 이미지" className={cn('productImage')} />
-        <h2 className={cn('productName')}>{name}</h2>
+        <div className={cn('productNameWrapper')}>
+          <h2 className={cn('nameEng')}>{nameEng}</h2>
+          <h2 className={cn('productName')}>{product}</h2>
+        </div>
       </div>
       <div className={cn('quantityContainer')}>
-        <QuantityButton quantity={quantity} />
+        <QuantityButton quantity={currentQuantity} increase={handleIncreaseClick} decrease={handleDecreaseClick} />
         <div className={cn('priceBox')}>
           <div className={cn('price')}>{price.toLocaleString()}원</div>
-          <button className={cn('deleteButton')} onClick={openModal}>
+          <button className={cn('deleteButton')} onClick={() => openModal(id)}>
             <Delete width={16} height={16} stroke={'#bfbfbf'} />
           </button>
         </div>
