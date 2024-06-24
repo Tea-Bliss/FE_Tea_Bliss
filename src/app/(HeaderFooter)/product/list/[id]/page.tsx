dcf6cb7e36507de61857e096fc0013561560355a
황준용 <wnsdyd3333@gmail.com>
2024-06-25 06:15:32 +0900

    design: 상세 페이지 디자인 수정

--- src/app/(HeaderFooter)/product/list/[id]/page.tsx
+++ src/app/(HeaderFooter)/product/list/[id]/page.tsx
@@ -1,5 +1,5 @@
 import ProductLayout from '@/components/page-layout/productLayout/ProductLayout';
 
-export default function ProductList() {
+export default function Product() {
   return <ProductLayout />;
 }
