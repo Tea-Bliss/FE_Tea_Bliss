2d3fa9e991ee043d9f0e60daafd535ca02fb1e67
황준용 <wnsdyd3333@gmail.com>
2024-06-24 20:25:57 +0900

    feat: 주문 상품 컴포넌트

---
+++ src/components/page-layout/orderLayout/components/OrderProduct/OrderProduct.tsx
@@ -0,0 +1,27 @@
+'use client';
+import classNames from 'classnames/bind';
+
+import Accordion from '@/components/common/Accordion/Accordion';
+import AccordionContent from '@/components/common/Accordion/AccordionContent';
+import AccordionTrigger from '@/components/common/Accordion/AccordionTrigger';
+import { SelectedItemsType } from '@/components/page-layout/cartLayout/components/CartView/CartView';
+import styles from '@/components/page-layout/orderLayout/components/OrderProduct/OrderProduct.module.scss';
+
+import OrderProductItem from '../OrderProductItem/OrderProductItem';
+
+const cn = classNames.bind(styles);
+export default function OrderProduct() {
+  const orderProductItem = JSON.parse(localStorage.getItem('selectedItems')!);
+  return (
+    <Accordion className={cn('accordionContainer')}>
+      <AccordionTrigger className={cn('trigger')} value="item1">
+        주문 상품
+      </AccordionTrigger>
+      <AccordionContent className={cn('content')} value="item1">
+        {orderProductItem.map((item: SelectedItemsType) => (
+          <OrderProductItem {...item} key={item.id} />
+        ))}
+      </AccordionContent>
+    </Accordion>
+  );
+}
